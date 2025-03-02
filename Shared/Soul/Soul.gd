2843cb41bcd5d84984096681495d96bbd8a07f51
Mobyr <mobyrm11@gmail.com>
2025-03-02 17:50:14 +0100

    update hearth input each frames, double gravity of blue hearth when down is hold

--- Shared/Soul/Soul.gd
+++ Shared/Soul/Soul.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 
 var speed = 190
 var motion := Vector2.ZERO
-var gravity = 3
+var gravity = 6 * 60
 
 var jump = Vector2(40, 240)
 
@@ -22,54 +22,53 @@ func _ready():
 	modulate = Color(1, 0, 0, 1)
 	changeMovement(currentFunction)
 
-func _physics_process(delta):
+func _process(delta: float):
 	if currentFunction:
+		inputList = [
+			int(Input.is_action_pressed("ui_right")),
+			int(Input.is_action_pressed("ui_left")),
+			int(Input.is_action_pressed("ui_down")),
+			int(Input.is_action_pressed("ui_up")),
+		]
+		input.x = inputList[0] - inputList[1]
+		input.y = inputList[2] - inputList[3]
 		call(currentFunction, delta)
 
 func changeMovement(value: String):
+	updateColor(value)
 	var negate = ["", "still"]
 	if !(currentFunction in negate) and !(value in negate):
 		var ghost_inst = ghost.instantiate()
 		self.add_child(ghost_inst)
 	currentFunction = value
 
+func updateColor(value: String):
+	if (value == "red"): modulate = Color(1, 0, 0, 1)
+	if (value == "blue"): modulate = Color(0, 0, 1, 1)
+
 func still(_delta):
 	set_velocity(Vector2.ZERO)
 	move_and_slide()
 
 func red(_delta):
-	modulate = Color(1, 0, 0, 1)
-	inputList = [
-		int(Input.is_action_pressed("ui_right")),
-		int(Input.is_action_pressed("ui_left")),
-		int(Input.is_action_pressed("ui_down")),
-		int(Input.is_action_pressed("ui_up")),
-		]
-	input.x = inputList[0] - inputList[1]
-	input.y = inputList[2] - inputList[3]
 	motion = speed * input
 	set_velocity(motion)
 	move_and_slide()
 
-func blue(_delta):
-	modulate = Color(0, 0, 1, 1)
-	inputList = [
-		int(Input.is_action_pressed("ui_right")),
-		int(Input.is_action_pressed("ui_left")),
-		int(Input.is_action_pressed("ui_up")),
-		]
+func blue(delta: float):
 	if not is_on_floor():
-		motion.y += gravity * 2
-	input.x = inputList[0] - inputList[1]
-	input.y = inputList[2]
-	motion.x = speed * input.x
+		motion.y += gravity * delta
+		if (inputList[2]):
+			motion.y += gravity * delta
+
+	motion.x = 190 * input.x
 
 	if is_on_floor():
 		motion.y = 0
-		if inputList[2]:
+		if inputList[3]:
 			motion.y = - jump.y
 	else:
-		if not inputList[2] and motion.y < - jump.x:
+		if not inputList[3] and motion.y < - jump.x:
 			motion.y = - jump.x
 
 	if is_on_ceiling() and motion.y < - jump.x:
